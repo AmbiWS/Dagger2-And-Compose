63db1a1a1aae3099cf91e12e206fb3ca893b0231
Mike K. <ambi.ws.studio@gmail.com>
2024-06-22 02:20:12 +0300

    Add todo task

--- app/src/main/java/com/ambiws/daggerandcompose/App.kt
+++ app/src/main/java/com/ambiws/daggerandcompose/App.kt
@@ -5,7 +5,7 @@ import com.ambiws.daggerandcompose.core.di.components.AppComponent
 import com.ambiws.daggerandcompose.core.di.components.DaggerAppComponent
 
 // TODO Handle contexts in di graph
-// TODO Add scopes feature within Dagger 2
+// TODO Analyze and optimize scopes (object leaks and creation flow)
 // TODO Implement caching feature (lists & room)
 // TODO Optimize DI graph
 // TODO Check VM recreation cases
