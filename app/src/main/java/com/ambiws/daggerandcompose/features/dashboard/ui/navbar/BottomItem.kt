e38336094b9d663d4761bd33102a28fd0d500e3d
Mike K. <ambi.ws.studio@gmail.com>
2024-09-26 14:38:00 +0300

    Add bottom nav controller and bottom item object

---
+++ app/src/main/java/com/ambiws/daggerandcompose/features/dashboard/ui/navbar/BottomItem.kt
@@ -0,0 +1,45 @@
+package com.ambiws.daggerandcompose.features.dashboard.ui.navbar
+
+@Suppress("unused")
+enum class BottomItem(
+    val rootId: Int,
+    val viewId: Int,
+    val iconResDefault: Int,
+    var iconResActive: Int,
+    val title: Int,
+    val colorTitleDefault: Int,
+    var colorTitleActive: Int,
+) {
+    HOME(
+        1,
+        R.id.ivHome,
+        R.drawable.ic_home_grey,
+        R.drawable.ic_home_selected,
+        R.string.home,
+        R.color.grey,
+        R.color.secondary
+    ),
+    LIST(
+        2,
+        R.id.ivList,
+        R.drawable.ic_list_grey,
+        R.drawable.ic_list_selected,
+        R.string.list,
+        R.color.grey,
+        R.color.secondary
+    ),
+    PROFILE(
+        3,
+        R.id.ivProfile,
+        R.drawable.ic_profile_grey,
+        R.drawable.ic_profile_selected,
+        R.string.profile,
+        R.color.grey,
+        R.color.secondary
+    );
+
+    companion object {
+        fun findByViewID(viewId: Int): BottomItem =
+            values().firstOrNull { it.viewId == viewId } ?: TOP
+    }
+}
