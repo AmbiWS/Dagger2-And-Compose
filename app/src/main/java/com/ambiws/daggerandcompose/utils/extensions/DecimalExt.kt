a3a5791145cf297d32279c9a43e0b4347822f212
Mike K. <ambi.ws.studio@gmail.com>
2025-07-28 17:42:25 +0300

    Improve profile feature ui

---
+++ app/src/main/java/com/ambiws/daggerandcompose/utils/extensions/DecimalExt.kt
@@ -0,0 +1,7 @@
+package com.ambiws.daggerandcompose.utils.extensions
+
+import android.content.res.Resources
+
+val Int.dp: Int get() = (Resources.getSystem().displayMetrics.density * this).toInt()
+
+val Float.dp: Float get() = (Resources.getSystem().displayMetrics.density * this)
