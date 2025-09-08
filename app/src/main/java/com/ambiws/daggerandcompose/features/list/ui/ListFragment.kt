5c105c16ca3e78b57d07ec578af98bf30ed758df
Mike K. <ambi.ws.studio@gmail.com>
2025-09-08 14:14:42 +0300

    Add state handling for list ui module

--- app/src/main/java/com/ambiws/daggerandcompose/features/list/ui/ListFragment.kt
+++ app/src/main/java/com/ambiws/daggerandcompose/features/list/ui/ListFragment.kt
@@ -1,12 +1,15 @@
 package com.ambiws.daggerandcompose.features.list.ui
 
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ambiws.daggerandcompose.base.BaseFragment
+import com.ambiws.daggerandcompose.base.UiState
 import com.ambiws.daggerandcompose.base.list.DefaultListDiffer
 import com.ambiws.daggerandcompose.databinding.FragmentListBinding
 import com.ambiws.daggerandcompose.features.list.ui.list.CharacterBaseItemModel
 import com.ambiws.daggerandcompose.features.list.ui.list.CharactersAdapterDelegate
 import com.ambiws.daggerandcompose.utils.extensions.subscribe
+import com.google.android.material.snackbar.Snackbar
 import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
 
 class ListFragment : BaseFragment<ListViewModel, FragmentListBinding>(
@@ -34,6 +37,30 @@ class ListFragment : BaseFragment<ListViewModel, FragmentListBinding>(
         super.setupObservers()
         subscribe(viewModel.charactersLiveData) {
             adapter.items = it
+        }
+        subscribe(viewModel.stateLiveEvent) {
+            when (it) {
+                is UiState.Error -> {
+                    binding.rvCharacters.isVisible = false
+                    binding.cpiLoader.isVisible = true
+                    Snackbar
+                        .make(
+                            requireContext(),
+                            binding.root,
+                            "${it.error} is occurred",
+                            Snackbar.LENGTH_SHORT
+                        )
+                        .show()
+                }
+                UiState.Loading -> {
+                    binding.rvCharacters.isVisible = false
+                    binding.cpiLoader.isVisible = true
+                }
+                UiState.Success -> {
+                    binding.rvCharacters.isVisible = true
+                    binding.cpiLoader.isVisible = false
+                }
+            }
         }
     }
 }
